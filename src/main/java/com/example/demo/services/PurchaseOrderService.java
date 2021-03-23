ebcb0df5e4e57ebe55a28e8fe659aaecad243baa
chaimaMannai <80989354+chaimaMannai@users.noreply.github.com>
2021-03-23 02:29:40 +0100

    Create PurchaseOrderService.java

---
+++ src/main/java/com/example/demo/services/PurchaseOrderService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import java.util.List;
+import com.example.demo.dto.PurchaseOrderDto;
+
+public interface PurchaseOrderService {
+	PurchaseOrderDto createPurchaseOrder(PurchaseOrderDto purchaseOrder,int id);
+	List<PurchaseOrderDto> getAllPurchaseOrder();
+	PurchaseOrderDto getPurchaseOrderById(int id);
+	PurchaseOrderDto modifyPurchaseOrder(int id, PurchaseOrderDto purchaseOrder);
+	PurchaseOrderDto deletePurchaseOrderById(int id);
+
+}
