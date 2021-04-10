55fa742475198d29a870a54752405c60005b8d57
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-04-11 01:21:13 +0200

    Create EmailService.java

---
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -0,0 +1,62 @@
+package com.example.demo.services;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+		@Autowired
+	    private JavaMailSender mailSender;
+	      
+	    @Autowired
+	    private SimpleMailMessage preConfiguredMessage;
+	  
+	    /**
+	     * This method will send compose and send the message 
+	     * */
+	  
+	    
+	    
+	    public void sendMailOrder(String to, String subject) 
+	    {
+	        MimeMessage msg = mailSender.createMimeMessage();
+	        String body = "<center><h1>Commande ajoutee</h1></center>";
+	        		
+	        MimeMessageHelper helper;
+			try {
+				helper = new MimeMessageHelper(msg, true);
+				helper.setTo(to);
+				helper.setSubject(subject);
+				helper.setText(body,true);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+	        
+	        
+	        mailSender.send(msg);
+	    }
+	    
+	    
+	
+	    
+	    
+	    
+	  
+	    /**
+	     * This method will send a pre-configured message
+	     * */
+	    public void sendPreConfiguredMail(String message) 
+	    {
+	        SimpleMailMessage mailMessage = new SimpleMailMessage(preConfiguredMessage);
+	        mailMessage.setText(message);
+	        mailSender.send(mailMessage);
+	    }
+
+}
