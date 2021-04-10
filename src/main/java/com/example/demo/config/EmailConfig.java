470749d4bdbada10b3f31a6b572fc02a14535a65
Khalilmeddeb2 <75492297+Khalilmeddeb2@users.noreply.github.com>
2021-04-11 01:20:56 +0200

    Create EmailConfig.java

---
+++ src/main/java/com/example/demo/config/EmailConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class EmailConfig {
+	@Bean
+    public SimpleMailMessage emailTemplate()
+    {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("khain@gmail.com");
+        message.setFrom("spring@gmail.com");
+        message.setSubject("Important email");
+        message.setText("FATAL - Application crash. Save your job !!");
+        return message;
+    }
+
+}
